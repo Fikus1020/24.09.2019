f508e6aed32965f02382e2c6c9123efa4f4feba0
Fikus1020 <45180885+Fikus1020@users.noreply.github.com>
2019-09-24 15:40:41 +0200

    Create logowanie.php

---
+++ logowanie.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+
+<?php
+    session_start();
+
+    if(isset($_GET['akcja'])&& $_GET['akcja']== 'wyloguj')
+    {
+        unset($_SESSION['zalogowany']);
+    }
+    if(!isset($_SESSION['zalogowany']))
+    {
+        ?>
+            <form action="plik.php" method="POST">
+                <input type="text" name="login" placeholder="login">
+                <input type="text" name="pass" placeholder="hasło">
+                <input type="submit" value="zaloguj">
+        </form> 
+        <?php
+    }else{
+        echo ('<li>ZALOGOWANY ');
+        echo '<li><a href="logowanie.php?akcja=wyloguj">Wyloguj</a>';
+        echo '<li><a href="html.html">plik1</a>';
+
+    }
+    
+
+    ?>
+    
+
+</body>
+</html>
